d5c9296885e2d3d3a9104c115182ea65df906042
David Siew <davidsiew@outlook.com>
2020-03-08 10:21:27 +0100

    Change the bash script to take in the msg

--- publish.sh
+++ publish.sh
@@ -2,5 +2,4 @@ dotnet paket restore
 tools/fsformatting.exe literate --processDirectory --inputDirectory "code" --outputDirectory "_posts"
 
 git add --all .
-git commit -a -m %1
-git push
+git commit -a -m "${1}"
