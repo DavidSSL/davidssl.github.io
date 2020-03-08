807f4e4c7dad124ad16ccf5973b10a3ff9fcceff
David Siew <davidsiew@outlook.com>
2020-03-08 11:34:32 +0100

    Complete intro

--- code/2020-03-08-Intro.fsx
+++ code/2020-03-08-Intro.fsx
@@ -4,9 +4,10 @@ layout: page
 title: Introduction to this blog
 ---
 
-
 (**
-Let's try again and see
+This is yet another blog but the focus of this is functional programming or at least my understanding of it.
+
+As a C# developer by day, it will come as no surprise that my functional language of choice is F#.
+
+Hopefully the information on here is useful to someone else other than myself.
 *)
-let add a b = a + b
-printfn "Hello@"
